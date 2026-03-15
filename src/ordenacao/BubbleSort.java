9318cbcf142f9b9addf14425be623cdeb8a0f4e8
Thais Ananda <thaisananda16@gmail.com>
2026-03-15 17:26:29 -0300

    adiciona classes BubbleSort e MergeSort

---
+++ src/ordenacao/BubbleSort.java
@@ -0,0 +1,31 @@
+package ordenacao;
+
+public class BubbleSort {
+	
+	public BubbleSort() {
+		super();
+	}
+	
+	public int[] bubbleSort(int[] vetor) {
+		
+		int tamanho = vetor.length;
+		
+		// Rodadas
+		for (int i = 0; i < tamanho - 1; i++) {
+			
+			System.out.println("Rodada #" + i);
+			
+			for (int j = 0; j < tamanho - 1 - i; j++) {
+				
+				if (vetor[j] > vetor[j + 1]) {
+					
+					int aux = vetor[j];
+					vetor[j] = vetor[j + 1];
+					vetor[j + 1] = aux;
+				}
+			}
+		}
+	
+		return vetor;
+	}
+}
